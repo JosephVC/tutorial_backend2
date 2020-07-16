2134a2713af398aeab4dadfb084c72e7de0cdb57
JosephVC <josephvcardenas@gmail.com>
2020-07-16 13:45:37 -0700

    created urls.py in ocr directory; created Post model

---
+++ ocr/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    file = models.FileField(upload_to='post_files', default='file')
+
+    def __str__(self):
+        return self.title 
+
+        
---
+++ ocr/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from .views import PostViews
+
+urlpatterns = [
+    path('', PostViews.as_view(), name='file-upload')
+]
