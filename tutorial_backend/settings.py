4942a17b04407993ee587998ee124fa845c39d5d
Joseph Cardenas <josephvcardenas@gmail.com>
2020-07-30 10:20:54 -0700

    secret key -> env.var; made config var in heroku

--- tutorial_backend/settings.py
+++ tutorial_backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # ----SECUIRITY---- 
-SECRET_KEY = os.environ.get('DJANGO_BACKEND_TUTORIAL_SECRET_KEY', '(93^ggrz8y&%rp6#8izypahg4^yuaf^@j=(v%(4=-5q29d=85j')
+SECRET_KEY = os.environ.get('DJANGO_BACKEND_TUTORIAL_SECRET_KEY', 'backend-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
