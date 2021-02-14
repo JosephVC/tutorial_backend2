6f498773a710b3c21024c68d7bf108b326060e57
Joseph Cardenas <josephvcardenas@gmail.com>
2021-02-13 20:22:14 -0800

    testing . . .

--- tutorial_backend/settings.py
+++ tutorial_backend/settings.py
@@ -146,6 +146,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+STATIC_ROOT = os.path.join(BASE_DIR, "static") 
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
